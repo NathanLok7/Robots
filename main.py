f9139522f4e9959b43a75fa6a8306b6bbce13954
Brandon Perez <brandon.perez@iteso.mx>
2024-09-13 15:49:12 -0600

    Actualizacion de codigo, control mejorado de colores v1.3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# Declaracion de objetos 
+# Declaracion de objetos y variables
 ev3 = EV3Brick()
 motor1 = Motor(Port.A,positive_direction=Direction.COUNTERCLOCKWISE,gears=[40,20])
 motor2 = Motor(Port.B,positive_direction=Direction.COUNTERCLOCKWISE,gears=[40,20])
@@ -18,35 +18,35 @@ ultrasonico = InfraredSensor(Port.S2)
 blancos = 0
 busqueda = True
 
-#Funcion para mover los motores simultaneamente
+# Funcion para mover los motores simultaneamente
 def mover_motores_adelante():
     motor1.run(10000)
     motor2.run(10000)
     motor3.run(10000)
     motor4.run(10000)
 
-#Funcion para parar los motores simultaneamente
+# Funcion para parar los motores simultaneamente
 def parar_motores():
     motor1.stop()
     motor2.stop()
     motor3.stop()
     motor4.stop()
 
-#Funcion para girar a la izquierda
+# Funcion para girar a la izquierda
 def izquierda():
     motor1.run(-3000)
     motor2.run(1800)
     motor3.run(-3000)
     motor4.run(1800)
 
-#Funcion para girar a la derecha
+# Funcion para girar a la derecha
 def derecha():
     motor1.run(3000)
     motor2.run(-1800)
     motor3.run(3000)
     motor4.run(-1800)
 
-#Funcion para girar por primera vez a la izquierda
+# Funcion para girar por primera vez a la izquierda
 def primer_izquierda():
     motor1.run(-4000)
     motor2.run(2800)
@@ -54,28 +54,30 @@ def primer_izquierda():
     motor4.run(2800)
     wait(1500)
 
-
+'''Girar aproximadamente 120 grados para intentar impactar al oponente por primera vez, si no se consigue igual se 
+ejecuta el ciclo infinito con el programa para la deteccion del oponente'''
 primer_izquierda()
 while True:
+    color = Sensor_luz.color()
+    print("Color: {}, Busqueda: {}, Blancos: {}".format(color,busqueda,blancos))
+
     if ultrasonico.distance() != 100: 
-        mover_motores_adelante()
         busqueda = True
         blancos = 0 
-        print("Busqueda: {}, Blancos: {}".format(busqueda,blancos))
-        while Sensor_luz.color() == Color.BLACK:
+        while Sensor_luz.color() == Color.BLACK or Sensor_luz.color() == Color.BLUE:
             mover_motores_adelante()
-    elif Sensor_luz.color() == Color.BLACK and ultrasonico.distance() == 100 and busqueda:
-        while Sensor_luz.color() == Color.BLACK:
+    elif (Sensor_luz.color() == Color.BLACK or Sensor_luz.color() == Color.BLUE) and ultrasonico.distance() == 100 and busqueda:
+        while Sensor_luz.color() == Color.BLACK or Sensor_luz.color() == Color.BLUE:
             mover_motores_adelante()
         busqueda = False
-    elif ultrasonico.distance() == 100 and Sensor_luz.color() != Color.BLACK and blancos ==  0:
+    elif ultrasonico.distance() == 100 and (Sensor_luz.color() != Color.BLACK and Sensor_luz.color() != Color.BLUE) and blancos == 0:
         parar_motores()
         while Sensor_luz.color() != Color.BLACK:
             izquierda()
         while Sensor_luz.color() == Color.BLACK and ultrasonico.distance() != 100:
             izquierda()
         blancos = 1
-    elif ultrasonico.distance() == 100 and Sensor_luz.color() != Color.BLACK and blancos == 1:
+    elif ultrasonico.distance() == 100 and (Sensor_luz.color() != Color.BLACK and Sensor_luz.color() != Color.BLUE) and blancos == 1:
         parar_motores()
         while Sensor_luz.color() != Color.BLACK:
             derecha()
